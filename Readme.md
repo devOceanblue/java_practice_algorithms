e93ccd0970fd0343443e1207b7be03fa870bbd86
kimseungyoon <neptuner24@loplat.com>
2023-03-01 21:25:37 +0900

    문제풀이

---
+++ Readme.md
@@ -0,0 +1,15 @@
+## Introduction
+알고리즘 문제풀이를 위한 깃허브 저장소입니다.
+
+알고리즘 문제의 종류는 
+1. Programmers
+2. Leetcode
+3. 백준
+
+세가지의 종류로 문제를 풀며 문제는 다음과 같이 분류됩니다.
+
+~~~
+Programmers의 문제는 클래스의 이름앞에 Problem...
+Leetcode의 문제는 영어원문(ex. ValidPalindrome)
+백준의 문제는 BOJ...
+~~~
---
+++ src/main/java/com/example/demo/ValidPalindrome.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+public class ValidPalindrome {
+    public boolean isPalindrome(String s) {
+        String sl = s.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
+        for(int i=0; i<sl.length(); i++){
+            if(sl.charAt(i) != sl.charAt(sl.length()-i-1)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
