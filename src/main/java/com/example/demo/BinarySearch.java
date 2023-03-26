639bdf8bbb14ee8461561282f34131f49c9f3c3c
kimseungyoon <neptuner24@loplat.com>
2023-03-26 22:59:24 +0900

    [leetcode] binarySearch

---
+++ src/main/java/com/example/demo/BinarySearch.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+public class BinarySearch {
+
+    public int binarySearch(int[] nums, int left, int right, int target) {
+        int mid = 0;
+        if (left <= right) {
+            mid = (left + right) / 2;
+            if (nums[mid] > target) {
+                return binarySearch(nums, left, mid, target);
+            } else if (nums[mid] < target) {
+                return binarySearch(nums, mid, right, target);
+            } else {
+                return mid;
+            }
+        }
+        else{
+            return -1;
+        }
+    }
+    public int search(int[] nums, int target) {
+        return binarySearch(nums, 0, nums.length-1, target);
+    }
+}
